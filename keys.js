8b96fbcfcdf9703533b1ddc65895a41c0e3742cb
Jennifer Bono <jennb24@gmail.com>
2017-11-17 09:57:16 -0600

    add files

---
+++ keys.js
@@ -0,0 +1,10 @@
+// console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'JhKlq9UmxMLWiP5lWrTRm7lPJ',
+  consumer_secret: 'QtWv7LVxStf2fDmMPkOSuYAtEBuJ8eso0rjPuBPfXYzS0uHndZ',
+  access_token_key: '3315348740-9la9lxo1lefSdYe0sHhlCZ53CYFGKv6XxUX0Qc3',
+  access_token_secret: 'arD6uRC4QMFOD8bcIiAW3OfP3PpcHluuHR12CcTgVX3ry',
+}
+
+module.exports = twitterKeys;
